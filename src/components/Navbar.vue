01855c98df14dde46c7e720678d9547fbf8589b0
peter.chen1024 <u0052017@gmail.com>
2022-05-24 18:02:49 +0800

    add navbar dropdown menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,23 +4,59 @@
   >
     <div class="container">
       <router-link class="navbar-brand ff-paytone" to="/">MetaWall</router-link>
-      <div class="d-flex align-items-center">
-        <Avatar size="30" :imgUrl="imgUrl" />
-        <h2
-          class="ff-azeret fs-6 fw-bold border-bottom border-2 border-dark ms-2 mb-0"
+
+      <div class="dropdown">
+        <div
+          class="d-flex align-items-center dropdown-toggle cursor-pointer"
+          data-bs-toggle="dropdown"
         >
-          Member
-        </h2>
+          <Avatar size="30" :imgUrl="imgUrl" />
+          <h2
+            class="ff-azeret fs-6 fw-bold border-bottom border-2 border-dark ms-2 mb-0"
+          >
+            Member
+          </h2>
+        </div>
+        <ul class="dropdown-menu text-center border border-2 border-dark py-0">
+          <li class="border-bottom border-2 border-dark">
+            <a
+              class="dropdown-item py-2"
+              href="#"
+              @click.prevent="router.push(`/user/${userStore.user._id}`)"
+              >我的貼文牆</a
+            >
+          </li>
+          <li class="border-bottom border-2 border-dark">
+            <a class="dropdown-item py-2" href="#">修改個人資料</a>
+          </li>
+          <li class="">
+            <a class="dropdown-item py-2" href="#" @click.prevent="logOut"
+              >登出</a
+            >
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
 </template>
 <script setup>
+import { useRouter } from 'vue-router';
 import Avatar from '@/components/Avatar.vue';
+import useUserStore from '@/stores/user';
+
+const router = useRouter();
+const userStore = useUserStore();
 
 defineProps({
   imgUrl: {
     type: String,
   },
 });
+
+const logOut = () => {
+  localStorage.removeItem('metaWall'); // 移除 token
+  router.push('/signin');
+};
 </script>
+
+<style lang="scss" scoped></style>
